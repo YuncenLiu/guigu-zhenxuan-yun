418b90afcbef8e9db7d1a605e05f7f27c55df07a
yun <array_xiangxiang@163.com>
2024-07-01 15:52:20 +0800

    feat: 添加环境变量并在组件中使用

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,7 +2,7 @@
   <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
   <el-button type="success" size="default" :icon="Edit">编辑按钮</el-button>
   <el-button type="danger" size="default" :icon="Delete">删除按钮</el-button>
-  <h2>{{data.VITE_APP_TITLE}}</h2>
+  <h2>{{ data.VITE_APP_TITLE }}</h2>
   <el-pagination
     :page-size="[100, 200, 300, 400]"
     :small="small"
@@ -15,11 +15,11 @@
 
 <script setup lang="ts">
 import { Plus, Edit, Delete } from '@element-plus/icons-vue'
-import {reactive} from "vue"
+import { reactive } from 'vue'
 
 let data = reactive(import.meta.env)
 
-console.log('test',import.meta.env)
+console.log('test', import.meta.env)
 </script>
 
 <style scoped></style>
