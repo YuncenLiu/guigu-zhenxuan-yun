a026b2fb0920c26cd798474585a37800bde18041
yun <array_xiangxiang@163.com>
2024-07-18 18:24:48 +0800

    feat: add

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,6 +1,6 @@
 //书写品牌管理模块接口
 import request from '@/utils/request'
-import { TradeMarkResponseData } from '@/api/product/trademark/type'
+import { TradeMarkResponseData, TradeMark } from '@/api/product/trademark/type'
 
 enum API {
   // 接口地址： http://139.198.104.58:8209/swagger-ui.html#/base45trademark45controller
@@ -18,3 +18,17 @@ export const reqHasTrademark = (page: number, limit: number) =>
   request.get<any, TradeMarkResponseData>(
     API.TRADEMARK_URL + `${page}/${limit}`,
   )
+//添加与修改已有品牌接口方法
+export const reqAddOrUpdateTrademark = (data: TradeMark) => {
+  //修改已有品牌的数据
+  if (data.id) {
+    return request.put<any, any>(API.UPDATETRADEMARK_URL, data)
+  } else {
+    //新增品牌
+    return request.post<any, any>(API.ADDTRADEMARK_URL, data)
+  }
+}
+
+//删除某一个已有品牌的数据
+export const reqDeleteTrademark = (id: number) =>
+  request.delete<any, any>(API.DELETE_URL + id)
